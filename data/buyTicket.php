3f8a4457305a4783690757081839be160f4cb5c2
liurengzhuang1997 <1849123057@qq.com>
2017-07-18 15:49:15 +0800

    1

---
+++ data/buyTicket.php
@@ -0,0 +1,25 @@
+<?php 
+	$type = $_REQUEST['type'];
+	$str = '';
+	$showtime=date("YndGis",time()+6*3600);
+	$date=date("Y-m-d",time()+6*3600);
+	if ($type == 'getMovieList') {//获取电影列表
+		$cId = $_REQUEST['cId'];//cid表示影院的cinemaId
+		$str = file_get_contents('http://m.mtime.cn/Service/callback.mi/Showtime/ShowtimeMovieAndDateListByCinema.api?cinemaId='.$cId.'&t='.$showtime);
+	}elseif ($type == 'getMovieTime') {//获取指定电影当天播放时间及第二天播放时间
+		$cId = $_REQUEST['cId'];//cid表示影院的cinemaId
+		$mId = $_REQUEST['mId'];//电影的movieID
+		$str = file_get_contents('http://m.mtime.cn/Service/callback.mi/showtime/ShowTimesByCinemaMovieDate.api?cinemaId='.$cId.'&movieId='.$mId.'&date='.$date.'&t='.$showtime);
+	}
+	elseif ($type == "getMovieDetail") {//电影详情
+		$mid = $_REQUEST['mid'];
+		$str = file_get_contents('http://m.mtime.cn/Service/callback.mi/movie/Detail.api?movieId='.$mId.'&locationId=290&t='.$showtime);
+	}elseif ($type == "HotLongComments") {//精选影评
+		$mid = $_REQUEST['mid'];
+		$str = file_get_contents('http://m.mtime.cn/Service/callback.mi/Movie/HotLongComments.api?movieId='.$mId.'&pageIndex=1&t='.$showtime);
+	}elseif ($type == 'MovieComments') {//网友短评
+		$mid = $_REQUEST['mid'];
+		$str = file_get_contents('http://m.mtime.cn/Service/callback.mi/Showtime/MovieComments.api?movieId='.$mId.'&pageIndex=1&t='.$showtime);
+	}
+	echo "$str";
+?>
